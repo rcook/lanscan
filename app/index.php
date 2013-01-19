625fb43f800f78c8f76d31a3a46e0c9bdd07bfd4
Richard Cook <rcook@rcook.org>
2013-01-18 19:42:39 -0800

    Tweaks

--- app/index.php
+++ app/index.php
@@ -7,6 +7,9 @@
 </head>
 <body>
 <h1>Lanscan</h1>
+<p>
+  This is the home of <em>Lanscan</em>, the simple network scanning tool available in the Windows Store.
+</p>
 </body>
 </html>
 
--- app/privacy.php
+++ app/privacy.php
@@ -1,2 +1,15 @@
-PRIVACY STATEMENT
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Lanscan</title>
+  <link rel="stylesheet" href="/default.css">
+</head>
+<body>
+<h1>Lanscan</h1>
+<p>
+  This is the home of <em>Lanscan</em>, the simple network scanning tool available in the Windows Store.
+</p>
+</body>
+</html>
 
