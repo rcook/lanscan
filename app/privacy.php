c8fdb7bf7b2dbd5e80f2e9b7086907fbdbe19bf8
Richard Cook <rcook@rcook.org>
2013-01-18 19:51:40 -0800

    Updated privacy policy

--- app/privacy.php
+++ app/privacy.php
@@ -2,14 +2,24 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Lanscan</title>
+  <title>Lanscan | Privacy policy</title>
   <link rel="stylesheet" href="/default.css">
 </head>
 <body>
-<h1>Lanscan</h1>
+<div id="nav">
+  <a href="/">Home</a>
+</div>
+<h1>Privacy</h1>
+<h2>Does Lanscan use any of your information?</h2>
 <p>
-  This is the home of <em>Lanscan</em>, the simple network scanning tool available in the Windows Store.
+  No. Lanscan scans ports. It does not collect any information from you nor does
+  it relay any personal information to our, or anybody else's, servers.
 </p>
+<h2>Do we use cookies?</h2>
+<p>
+  No.
+</p>
+<h2>Is that really it?</h2>
+Yes. Lanscan is a port-scanning tool. Nothing else.
 </body>
 </html>
-
