50ddb7b2c065e8725b868b9a0d09f349c0a618f5
Richard Cook <rcook@rcook.org>
2013-01-21 10:41:21 -0800

    Added link to install app

--- app/index.php
+++ app/index.php
@@ -11,6 +11,9 @@
   This is the home of Lanscan, a simple network scanning tool available in the Windows Store.
 </p>
 <p>
+  <a href="http://apps.microsoft.com/windows/app/lanscan/23324308-b07d-471b-b6d5-24ac9a0bb595">Click here to install this app from the Windows Store</a>
+</p>
+<p>
   <a href="/privacy">Click on this link right here to read our privacy policy.</a>
 </p>
 <? require_once 'footer.php'; ?>
