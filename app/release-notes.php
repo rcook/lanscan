64ad603fd5f976997a19437dfbca2ac9634bdb02
Richard Cook <rcook@rcook.org>
2013-01-26 15:32:16 -0800

    Tweaked release notes

--- app/release-notes.php
+++ app/release-notes.php
@@ -13,24 +13,30 @@
        alt="Lanscan logo" />
 </div>
 <h1>Release notes</h1>
-<h2>Release 1</h2>
-<h2>Release 2</h2>
-<p>Bug fixes:</p>
+<h2>Release 2 v1.2.0.3 (2013/1/25)</h2>
+<h3>Bug fixes</h3>
 <ul>
-  <li>Obtain CIDR/prefix length from system network information instead of inferring it from IP address range</li>
+  <li>CIDR/network prefix length now obtained from system network information instead
+  of trying to infer it from the device's IP address</li>
   <li>Removed individual endpoint trace during scan as it hobbled responsiveness of UI</li>
+  <li>Fixed cancellation behaviour</li>
 </ul>
-<p>Features:</p>
+<h3>New features</h3>
 <ul>
   <li>Added gateway address to network info display</li>
   <li>Added DNS server address to network info display</li>
   <li>Added domain name to network info display</li>
   <li>Implemented reverse DNS to resolve IP addresses to FQDNs using local DNS server</li>
-  <li>Changed selection mode of network map control to "none" since there is no point in allowing selection: click is still supported</li>
-  <li>Perform external IP address lookup using http://whatismyipaddress.com/ and http://checkip.dyndns.org/</li>
+  <li>Changed selection mode of network map control to "none" since there is no point
+  in allowing selection: click for port 80/443 is still supported</li>
+  <li>Performs external IP address lookup using <a href="http://whatismyipaddress.com/">http://whatismyipaddress.com/</a>
+  and <a href="http://checkip.dyndns.org/"http://checkip.dyndns.org/"</a></li>
   <li>Added app bars and cleaned up UI</li>
 </ul>
+<h2>Release 1 (v1.1.0.2 (2013/1/18)</h2>
+<p>
+  Initial release.
+</p>
 <? require_once 'footer.php'; ?>
 </body>
 </html>
-
